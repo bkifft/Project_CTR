7e645b2b17f153bb1c2b9d4269f727cce659be5c
Jack <jack@azulon.net>
2023-04-01 09:29:39 +0800

    [ctrtool] Bump version to 1.2.0

--- ctrtool/src/version.h
+++ ctrtool/src/version.h
@@ -2,6 +2,6 @@
 #define APP_NAME	"CTRTool"
 #define BIN_NAME	"ctrtool"
 #define VER_MAJOR	1
-#define VER_MINOR	1
-#define VER_PATCH	1
+#define VER_MINOR	2
+#define VER_PATCH	0
 #define AUTHORS		"jakcron"
